4b69f607056c2631ace91cc5079dc5975eb15702
Ali taqi <34040786+Alitaqi@users.noreply.github.com>
2025-11-14 13:00:22 +0500

    Refactor CORS origin configuration to array format

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-          "https://repotics.vercel.app",
+  origin: [
+    "http://localhost:5173",
+    "https://repotics.vercel.app"
+  ],
   credentials: true,
 }));
 
