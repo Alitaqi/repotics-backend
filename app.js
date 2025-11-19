e4f641f7e4488e59c7d117b5538f007aaf3eff7c
Synctom-Softwares <services@synctom.com>
2025-11-19 16:29:26 +0500

    helo 1st commit from latop

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://repotics.vercel.app",
-     "https://repotics-backend.onrender.com"
+    "https://repotics-backend.onrender.com"
   ],
   credentials: true,
 }));
